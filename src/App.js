eada343da05da4cb9d69935db8b4deb9a25eafcd
Dmitry-Yemelin <dmitry.yemelin@gmail.com>
2023-12-07 00:31:26 +0000

    Add new blog version 2

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog VERSION 1
+            EKS DEMO Blog version 2 !!! security
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
